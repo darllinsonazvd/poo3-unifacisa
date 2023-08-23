f6bd0cb4bd29e08d773f1b372ef547e47f595456
Darllinson Azevedo <azevedo.darllinson@gmail.com>
2023-08-23 19:30:30 -0300

    format code of aula4-json-1

--- aula4-json-1/script.js
+++ aula4-json-1/script.js
@@ -1,15 +1,10 @@
 const fruit = {
-    "nome" : "banana",
-    "preco" : 3.5,
-    "tipo" : [
-        "prata",
-        "maça",
-        "da terra",
-        "nanica"
-    ],
-    "origem": "brasil"
+  nome: 'banana',
+  preco: 3.5,
+  tipo: ['prata', 'maça', 'da terra', 'nanica'],
+  origem: 'brasil',
 }
 
 const banana = () => {
-    console.log(fruit)
+  console.log(fruit)
 }

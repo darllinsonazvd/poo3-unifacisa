d1634beb01564f965029f1767217a3ec9be65547
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-08-23 19:25:00 -0300

    last commit

---
+++ aula4-json-1/script.js
@@ -0,0 +1,15 @@
+const fruit = {
+    "nome" : "banana",
+    "preco" : 3.5,
+    "tipo" : [
+        "prata",
+        "maça",
+        "da terra",
+        "nanica"
+    ],
+    "origem": "brasil"
+}
+
+const banana = () => {
+    console.log(fruit)
+}
